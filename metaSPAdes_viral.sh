6e091a0a3b75be58f56e714b094981a6ddf3912b
Marcopterix <91996285+marco315176@users.noreply.github.com>
2024-07-09 12:40:23 -0600

    Add files via upload

--- metaSPAdes_viral.sh
+++ metaSPAdes_viral.sh
@@ -38,7 +38,7 @@ rm -R /home/secuenciacion_cenasa/Analisis_corridas/SPAdes_viral/${ID}_metaSPAdes
 # Ejecuta seqtk sobre "${ID}-metaSPAdes-assembly.fasta" para eliminar todos los contigs menores a 450 pb y nombra el archivo de salida como "${ID}-metaSPAdes-assembly.fa"
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-seqtk seq -L 600 /home/secuenciacion_cenasa/Analisis_corridas/SPAdes_viral/${ID}-metaSPAdes-assembly.fasta > /home/secuenciacion_cenasa/Analisis_corridas/SPAdes_viral/${ID}-metaSPAdes-assembly.fa
+seqtk seq -L 300 /home/secuenciacion_cenasa/Analisis_corridas/SPAdes_viral/${ID}-metaSPAdes-assembly.fasta > /home/secuenciacion_cenasa/Analisis_corridas/SPAdes_viral/${ID}-metaSPAdes-assembly.fa
 
 chmod -R 775 /home/secuenciacion_cenasa/Analisis_corridas/SPAdes_viral/${ID}-metaSPAdes-assembly.fa
 
@@ -50,32 +50,6 @@ rm /home/secuenciacion_cenasa/Analisis_corridas/SPAdes_viral/${ID}-metaSPAdes-as
 
 done #término del ciclo iniciado con "for"
 
-
-echo -e "###########################################################################################"
-echo -e ===== Detección de genomas virales con Virsorter2 en ensambles obtenidos con metaSPAdes =====
-echo -e                                   ===== Inicio: $(date) =====
-echo -e "###########################################################################################"
-
-cd /home/secuenciacion_cenasa/Analisis_corridas/SPAdes_viral
-
-for ensamble in *.fa; do
-ID="$(basename ${ensamble} | cut -d '-' -f '1')"
-
-# -----------------------------------------------------------------------
-# Ejecutar virsorter2 para identificar los contigs pertenecientes a virus 
-# -----------------------------------------------------------------------
-
-virsorter run -w /home/secuenciacion_cenasa/Analisis_corridas/SPAdes_viral/Contigs_filtrados/${ID}_out_tmp \
-              -d $VIRSORTER2_DB_PATH \
-              -i ${ensamble} \
-              --min-score 0.1 \
-              --include-groups  dsDNAphage,NCLDV,RNA,ssDNA,lavidaviridae all
-
-mv /home/secuenciacion_cenasa/Analisis_corridas/SPAdes_viral/Contigs_filtrados/${ID}_virsorter_out/final-viral-combined.fa /home/secuenciacion_cenasa/Analisis_corridas/SPAdes_viral/Contigs_filtrados/${ID}_virsorter_out/${ID}-metaSPAdes-viral.fa
-mv /home/secuenciacion_cenasa/Analisis_corridas/SPAdes_viral/Contigs_filtrados/${ID}_virsorter_out/${ID}-metaSPAdes-viral.fa /home/secuenciacion_cenasa/Analisis_corridas/SPAdes_viral/Contigs_filtrados/.
-rm -R /home/secuenciacion_cenasa/Analisis_corridas/SPAdes_viral/Contigs_filtrados/${ID}_virsorter_out
-
-done
 
 echo -e "##################################################################"
 echo -e                   ===== Fin: $(date) =====
