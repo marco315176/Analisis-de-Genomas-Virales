044870ae70d9b89ac805162900c9c28c65050cd5
Marcopterix <91996285+marco315176@users.noreply.github.com>
2025-10-23 12:22:02 -0600

    Update bracken_virus.sh

--- bracken_virus.sh
+++ bracken_virus.sh
@@ -7,9 +7,9 @@ echo -e "#######################################################################
 
 #---------------------------------------------------------
 # Definir rutas de directorios de entrada y salida
-dirfq="/home/admcenasa/Analisis_corridas/Archivos_postrim/virus"
-dirout="/home/admcenasa/Analisis_corridas/kraken2/virus"
-dirbrackout="/home/admcenasa/Analisis_corridas/Bracken/virus"
+dirfq="/home/user/Analisis_corridas/Archivos_postrim/virus"
+dirout="/home/user/Analisis_corridas/kraken2/virus"
+dirbrackout="/home/user/Analisis_corridas/Bracken/virus"
 #---------------------------------------------------------
 
 cd ${dirfq}
